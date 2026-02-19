8f7e24f4c375bf4acb1801d74621327a17714378
Voimaaa <104168513+Voimaaa@users.noreply.github.com>
2026-02-19 16:46:56 +0100

    Update server.js
    Removing "cors".

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.use(cors());
-
 app.get('/UserEmail', (req, res) => {
     const email = req.query.email;
     res.json({ email });
@@ -13,3 +11,4 @@ app.get('/UserEmail', (req, res) => {
 app.listen(port, () => {
   console.log(`Scoutagent Server API is running.`)
 })
+
