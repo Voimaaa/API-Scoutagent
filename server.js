35797878ce65c89ad8769ab7e6c890eeee150be5
Voimaaa <104168513+Voimaaa@users.noreply.github.com>
2026-02-19 16:55:59 +0100

    Update server.js
    Adding new Features for the Storage-Folder Creation.

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const { execSync } = require('child_process');
 const app = express()
 const port = 8080
 
-app.get('/UserEmail', (req, res) => {
+app.get('/createStorageFolder', (req, res) => {
     const email = req.query.email;
     res.json({ email });
     console.log("Folgende Email vom User wurde erhalten: " + email);
+
+    try {
+      execSync(`node create-storage-folder.js "${email}"`, { stdio: 'pipe' });
+    } catch {
+      res.status(500).json({ error: error.message });
+    }
 })
 
 app.listen(port, () => {
   console.log(`Scoutagent Server API is running.`)
 })
-
